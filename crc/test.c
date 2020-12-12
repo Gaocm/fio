e96db674480f584605755c06725b427dd47d9637
gaocm <albertgaocm@gmail.com>
2020-12-12 15:20:48 +0800

    add compressable buffer

--- crc/test.c
+++ crc/test.c
@@ -106,8 +106,8 @@ static void t_crc7(struct test_type *t, void *buf, size_t size)
 	int i;
     LZ4_stream_t ctx;
     LZ4_stream_t* const ctxPtr = &ctx;
-    char *out='temp';
-
+    char *out;
+    out = malloc(CHUNK);
 
 //	for (i = 0; i < NR_CHUNKS; i++)
         LZ4_compress_fast_extState(ctxPtr, buf, out, size, size, 0);
