5c2f692a1773d29f2300bef792614c9f25783850
gaocm <albertgaocm@gmail.com>
2020-12-12 09:57:37 +0800

    add compressable buffer

--- crc/test.c
+++ crc/test.c
@@ -378,7 +378,7 @@ int fio_crctest(const char *type)
 	struct frand_state state;
 	int i, first = 1;
 	void *buf;
-	void *temp;
+	char *temp;
 
 	crc32c_arm64_probe();
 	crc32c_intel_probe();
@@ -398,7 +398,7 @@ int fio_crctest(const char *type)
 	buf = malloc(CHUNK);
 	init_rand_seed(&state, 0x8989, 0);
 	//fill_random_buf(&state, buf, CHUNK);
-    fill_random_buf_percentage(&state, buf, 80, 4096, 4096, '0', 0);
+    fill_random_buf_percentage(&state, buf, 80, 4096, 4096, temp, 0);
 
 
 	for (i = 0; t[i].name; i++) {
