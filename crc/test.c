453c10695e4addc667b7b72ba75bd6af4e63c1cc
gaocm <albertgaocm@gmail.com>
2020-12-12 10:53:16 +0800

    add compressable buffer

--- crc/test.c
+++ crc/test.c
@@ -404,7 +404,7 @@ int fio_crctest(const char *type)
 	buf = malloc(CHUNK);
 	init_rand_seed(&state, 0x8989, 0);
 	fill_random_buf(&state, buf, CHUNK);
-    fill_random_buf_percentage(&state, buf, perc, this_write, this_write, temp, 0);
+    //fill_random_buf_percentage(&state, buf, perc, this_write, this_write, temp, 0);
 
 	for (i = 0; t[i].name; i++) {
 		struct timespec ts;
