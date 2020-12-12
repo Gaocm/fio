95310999e8316572c970c9ae7d679121ec4cd950
gaocm <albertgaocm@gmail.com>
2020-12-12 11:38:40 +0800

    add compressable buffer

--- crc/lz4.c
+++ crc/lz4.c
@@ -1039,7 +1039,7 @@ LZ4_FORCE_INLINE int LZ4_compress_generic_validated(
             if (litLength >= RUN_MASK) {
                 printf("start compress run mask 1\n");
                 int len = (int)(litLength - RUN_MASK);
-                printf("start compress run mask 2\n");
+                printf("start compress run mask 2\t%s\n",*token);
                 *token = (RUN_MASK<<ML_BITS);
                 printf("start compress run mask 3\n");
                 for(; len >= 255 ; len-=255) *op++ = 255;
