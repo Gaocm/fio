6d0db10ca124dd3056626f846f1fe61b9e643127
gaocm <albertgaocm@gmail.com>
2020-12-12 13:14:10 +0800

    add compressable buffer

--- crc/lz4.c
+++ crc/lz4.c
@@ -1035,8 +1035,8 @@ LZ4_FORCE_INLINE int LZ4_compress_generic_validated(
             if (litLength >= RUN_MASK) {
                 int len = (int)(litLength - RUN_MASK);
                 token = (RUN_MASK<<ML_BITS);
-                for(; len >= 255 ; len-=255) op++ = 255;
-                op++ = (BYTE)len;
+                for(; len >= 255 ; len-=255) op++ = (unsigned)255;
+                op++ = len;
             }
             else *token = (BYTE)(litLength<<ML_BITS);
 
