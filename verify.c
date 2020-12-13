41feaa8c1e9e32870696c0d0850f3625d1748e7e
gaocm <albertgaocm@gmail.com>
2020-12-13 17:26:11 +0800

    modify crctest

--- verify.c
+++ verify.c
@@ -1289,6 +1289,7 @@ static void populate_hdr(struct thread_data *td, struct io_u *io_u,
 	if (td->o.verify_offset && hdr_size(td, hdr))
 		memswp(p, p + td->o.verify_offset, hdr_size(td, hdr));
     free(out);
+    free(ctx);
 }
 
 /*
